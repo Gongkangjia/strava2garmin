9187fafa6606f8af33363db5f99f8e19d38086a7
Gongkangjia <gongkangjia@qq.com>
2024-12-24 13:27:10 +0800

    返回状态码_post

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 
 
-@app.get("/")
-async def print_request(request: Request):
+@app.post("/")
+async def root(request: Request):
     body = await request.json()
     print("Received request body:", body)
     return JSONResponse(
         content={"message": "Request received successfully"}, status_code=200
     )
+
+
+@app.get("/")
+async def root(request: Request):
+    body = await request.json()
+    print("Received request body:", body)
+    return JSONResponse(content={"message": "get successfully"}, status_code=200)
