5cf76074ac080240eff27a1443e28aa8e288b6fc
Gongkangjia <gongkangjia@qq.com>
2024-12-24 14:06:06 +0800

    bark

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import json
+import requests
 
 app = FastAPI()
 
@@ -9,7 +10,8 @@ app = FastAPI()
 @app.post("/")
 async def root(request: Request):
     body = await request.json()
-    print("Received request body:", body)
+    requests.get("https://api.day.app/JbPripgUKKNzbELvwURvrD/{body}")
+
     return JSONResponse(
         content={"message": "Request received successfully"}, status_code=200
     )
@@ -18,8 +20,6 @@ async def root(request: Request):
 @app.get("/")
 async def root(request: Request):
     data = request.query_params
-    print(data)
-
     if "hub.mode" in data:
         if (
             data["hub.mode"] != "subscribe"
